5db35be545dc6a2584ea6530bd4999a89caaa256
oran2527 <orlago250183@gmail.com>
2020-02-14 14:46:34 +0000

    point 4 dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 """ module point 1 base class """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """class BaseModel"""
 
-    
-    def __init__(self, my_number=0, name=""):
+
+    def __init__(self, *args, **kwargs):
         """init function"""
-        self.id = str(uuid.uuid4())
-        self.my_number = my_number
-        self.name = name
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs and kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(value, "\
+%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(value, "\
+%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """str function"""
@@ -23,13 +33,16 @@ class BaseModel:
 
     def to_dict(self):
         """to_dict function"""
+        n_dic = dict(self.__dict__)
+
         update = self.updated_at
         create = self.created_at
 
-        return {'my_number': self.my_number, 'name': self.name, '__class__\
-': self.__class__.__name__, 'updated_at\
-': update.strftime("%Y-%m-%dT%H:%M:%S.%f"), 'id': self.id, 'created_at\
-': create.strftime("%Y-%m-%dT%H:%M:%S.%f")}
+        n_dic["__class__"] = self.__class__.__name__
+        n_dic["created_at"] = create.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        n_dic["updated_at"] = update.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        return n_dic
 
     def save(self):
         """save function"""
