6592941fea04e4f983015e3815914f03428bf029
oran2527 <orlago250183@gmail.com>
2020-02-17 14:24:48 +0000

    point 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module point 1 base class """
 import uuid
+import models
 from datetime import datetime
 
 
@@ -10,34 +11,35 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """init function"""
-        if kwargs and kwargs != {}:
+        if kwargs != {}:
             for key, value in kwargs.items():
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(value, "\
+                    value = datetime.strptime(value, "\
 %Y-%m-%dT%H:%M:%S.%f")
                 if key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(value, "\
+                    value = datetime.strptime(value, "\
 %Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = value
+                self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
 
     def __str__(self):
         """str function"""
-        return "[{}] ({}) {}\
+        s = "[{}] ({}) {}\
 ".format(self.__class__.__name__, self.id, self.__dict__)
+        return s
 
     def to_dict(self):
         """to_dict function"""
-        n_dic = dict(self.__dict__)
-
+        
         update = self.updated_at
         create = self.created_at
 
+        n_dic = dict(self.__dict__)
         n_dic["__class__"] = self.__class__.__name__
         n_dic["created_at"] = create.strftime("%Y-%m-%dT%H:%M:%S.%f")
         n_dic["updated_at"] = update.strftime("%Y-%m-%dT%H:%M:%S.%f")
@@ -46,4 +48,5 @@ class BaseModel:
 
     def save(self):
         """save function"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.save()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python3
 """ module point 5 file_storage """
+import json
+import models
+from models.base_model import BaseModel
 
 
 class FileStorage:
     """class FileStorage"""
 
-    def __init__(self):
-        """init function""" 
-        self.__file__path = "file.json"
-	self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """all function"""
+
         return self.__objects
 
-    	
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+
+        objName = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[objName] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+
+        n_dict = {}
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            for key, value in self.__objects.items():
+                n_dict[key] = value.to_dict()
+            json.dump(n_dict, f)
+
+    def reload(self):
+        """deserializes JSON file to __objects"""
+        try:
+            with open(self.__file_path, "r", encoding='utf-8') as f:
+                o_dict = json.load(f)
+                for key, value in o_dict.items():
+                    value.pop("__class__")
+                    bmObj = BaseModel(**value)
+                    self.new(bmObj)
+        except:
+            pass
